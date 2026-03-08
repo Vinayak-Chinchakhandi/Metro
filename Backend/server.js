651e64166f22e5fd24029480896a9c5693a58161
Prajakta <prajaktapol45@gmail.com>
2026-03-08 17:51:38 +0530

    Backend APIs ready to be Tested by integrating with database schema

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,6 @@
 const app = require("./src/app");
 const { PORT } = require("./src/config/env");
 
-const createTables = require("./src/database/queries");
-
-// Create database tables when server starts
-createTables();
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,6 +1,9 @@
 const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
 
-const db = new sqlite3.Database("./src/database/metro.db", (err) => {
+const dbPath = path.join(__dirname, "../../../database/metro.db");
+
+const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
     console.error("Database error", err);
   } else {
