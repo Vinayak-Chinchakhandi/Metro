8a36d30a272f218df978eda44e64854c9dad42c1
Vinayak Chinchakhandi <vinayakchinchakhandi165@gmail.com>
2026-03-21 11:14:51 +0530

    updated navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ function Navbar() {
     <div className="w-full bg-indigo-700 text-indigo-100 px-8 py-4 flex justify-between items-center shadow-md">
 
       <h2 className="text-lg font-semibold tracking-wide hover:text-white transition duration-200">
-        Metro AI Ticketing System
+        AI Metro Ticketing System
       </h2>
 
       <div className="flex items-center gap-2 text-sm text-green-300 font-medium">
